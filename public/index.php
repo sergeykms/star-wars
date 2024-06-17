1a348c2c183d4cc187d0629af4b9e36fe170af8f
Sergey <serpp@me.com>
2024-06-17 22:27:05 +1000

    create project

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+phpinfo();
+
+//
+//<!DOCTYPE html>
+//<html lang="en">
+//<head>
+//    <meta charset="UTF-8">
+//    <title>Title</title>
+//</head>
+//<body>
+//<h1>Привет, PHP!</h1>
+//</body>
+//</html>
